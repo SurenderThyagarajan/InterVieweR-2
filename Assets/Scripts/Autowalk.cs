8f71e9dc7816d1db611b298954e233389c54437c
SurenderThyagarajan <tsuren1998@outlook.com>
2020-07-12 19:56:39 +0530

    First Commit

---
+++ Assets/Scripts/Autowalk.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VR;
+
+public class Autowalk : MonoBehaviour
+{
+    public AudioSource audioPlay;
+    public Text display;
+    public bool socketReady = false;
+    public Transform vrcamera;
+    public float toggle =30.0f;
+    public float speed = 3.0f;
+    public bool moveForward;
+    public TcpClient mySocket;
+    public NetworkStream theStream;
+    public StreamWriter theWriter; 
+    
+    public CharacterController cc;
+    public Button subResBtn;
+    public Button subFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        cc = GetComponentInParent<CharacterController>();
+        audioPlay = gameObject.GetComponent<AudioSource>();
+        subResBtn = gameObject.GetComponent<Button>();
+        subFire = gameObject.GetComponent<Button>();
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vrcamera.eulerAngles.x >=  toggle  && vrcamera.eulerAngles.x < 90.0f)
+        {
+            moveForward = true;
+            System.Console.WriteLine("Hello");
+        }
+        else
+            moveForward = false;
+        if (moveForward)
+        {
+            Vector3 fwd = vrcamera.TransformDirection(Vector3.forward);
+            cc.SimpleMove(fwd * speed);
+        }
+        else
+        {
+            Vector3 fwd = vrcamera.TransformDirection(Vector3.forward);
+            cc.SimpleMove(fwd * 0);
+        }
+    }
+
+  
+    public void OnTriggerEnter(Collider other)
+    {
+        speed = 0.0f;
+
+    }
+    
+
+    public void buttonClick()
+    {
+        setupSocket();
+        
+    }
+    
+    public void submitBtn()
+    {
+        try
+        {
+            mySocket = new TcpClient("192.168.137.1", 65432);
+            theStream = mySocket.GetStream();
+            theWriter = new StreamWriter(theStream);
+
+            socketReady = true;
+            writeSocket("submit");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error:" + e);
+        }
+    }
+
+    public void queBtn()
+    {
+        try
+        {
+            mySocket = new TcpClient("192.168.137.1", 65432);
+            theStream = mySocket.GetStream();
+            theWriter = new StreamWriter(theStream);
+
+            socketReady = true;
+            writeSocket("recieve");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error:" + e);
+        }
+    }
+    public void setupSocket()
+    {
+        try
+        {
+            mySocket = new TcpClient("192.168.137.1", 65432);
+            theStream = mySocket.GetStream();
+            theWriter = new StreamWriter(theStream);
+            
+            socketReady = true;
+            writeSocket("record");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error:" + e);
+        }
+    }
+
+    public void writeSocket(string theLine)
+    {
+        if (!socketReady)
+            return;
+        String tmpString = theLine;
+        Debug.Log(tmpString);
+        theWriter.Write(tmpString);
+        theWriter.Flush();
+        mySocket.Close();
+        Thread.Sleep(8000);
+        displayMsg();
+    }
+
+    public void displayMsg()
+    {
+        Debug.Log("After t secs");
+        string msg = "";
+        connection cn = new connection();
+
+        msg = cn.Connection();
+        Debug.Log(msg);
+
+        display.text = msg;
+        StartCoroutine(Music(msg));
+    }
+    IEnumerator  Music(String msg)
+       {
+       Debug.Log("Hello world");
+           Regex regex = new Regex("\\s+");
+           string res = regex.Replace(msg, "+");
+           string url = "https://api.voicerss.org/?key=6a9b31137cd84b1d98970e0ffc078d50&hl=en-gb&f=44khz_16bit_stereo&src=" + res;
+           WWW www = new WWW(url);
+           yield return www;
+           audioPlay.clip = www.GetAudioClip(false, true, AudioType.WAV);
+           audioPlay.Play();
+       }
+
+
+
+
+
+}
---
+++ Assets/Scripts/connection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class connection : MonoBehaviour
+{
+
+   
+    public string word = "";
+
+    // Start is called before the first frame update
+
+    public void Start()
+    {
+        
+        
+
+    }
+
+
+    public String Connection()
+    {
+        String msg = "";
+        Debug.Log("Hello again");
+        try
+        {
+            IPAddress ipAddress = IPAddress.Parse("192.168.137.1");
+            Debug.Log(ipAddress.ToString());
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                      SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 1024);
+            sender.Connect(localEndPoint);
+            Debug.Log(ipAddress.ToString() + " Connected");
+
+            byte[] messageReceived = new byte[1024];
+            int byteRecv = sender.Receive(messageReceived);
+            msg = Encoding.ASCII.GetString(messageReceived, 0, byteRecv);
+
+            Debug.Log("connected");
+            Debug.Log(msg);
+           
+            return msg;
+
+        }
+
+
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+            return "Please retry the action";
+
+        }
+    }
+    
+       
+        
+    
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+}
